0a4c3fc64df684a914372e2f1f443a8e13f957ce
Tyler Heintz <tyler@Tylers-MacBook-Air.local>
2018-06-14 12:17:22 -0700

    Add some learning code

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ const conn = new Connection({
   endpoint: 'http://localhost:5820',
 });
  
-query.execute(conn, 'myDB', 'select distinct ?s where { ?s ?p ?o }', 'application/sparql-results+json', {
-  limit: 10,
-  offset: 0,
-}).then(({ body }) => {
-  console.log(body.results.bindings);
-});
+// query.execute(conn, 'myDB', 'select distinct ?s where { ?s ?p ?o }', 'application/sparql-results+json', {
+//   limit: 10,
+//   offset: 0,
+// }).then(({ body }) => {
+//   console.log(body.results.bindings);
+// }); 
+
+
+function send_query(query_string) {
+    //param: query_string = String with full sparql query contained
+    //executes given sparql query and logs response
+    query.execute(conn, 'myDB', query_string, 'application/sparql-results+json', {
+        limit: 10,
+        offset: 0,
+      }).then(({ body }) => {
+        console.log("early results:" + body.results.bindings);
+        return body.results.bindings;
+      });
+}
+
+var print_val = send_query("select distinct ?s where { ?s ?p ?o }")
+console.log("end results: " + print_val)
