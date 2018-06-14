f66945f39835b109e61742ccde75182673368bdb
Nikhil Patel <n.patel@berkeley.edu>
2018-06-14 11:04:46 -0700

    Add boilerplate code for connecting to Stardog

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const { Connection, query } = require('stardog');
+ 
+const conn = new Connection({
+  username: 'admin',
+  password: 'admin',
+  endpoint: 'http://localhost:5820',
+});
+ 
+query.execute(conn, 'myDB', 'select distinct ?s where { ?s ?p ?o }', 'application/sparql-results+json', {
+  limit: 10,
+  offset: 0,
+}).then(({ body }) => {
+  console.log(body.results.bindings);
+});
